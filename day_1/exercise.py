b9fe4ecee90fac6fb785c7a297d4ebe10dba6d35
Kishan Kulu <kishankulu133@gmail.com>
2021-09-13 20:04:30 -0700

    update

--- day_1/exercise.py
+++ day_1/exercise.py
@@ -7,6 +7,7 @@
 import requests
 import json
 import openpyxl
+from openpyxl.styles import Font
 
 
 data = requests.get('https://data.messari.io/api/v2/assets')
@@ -19,20 +20,39 @@ roi = beautify['data'][0]['metrics']['roi_data']['percent_change_last_1_week']
 # print(roi)
 # print(symbol)
 
+
+wb = openpyxl.load_workbook('C:\\Users\\GorkhaliSquad\\Documents\\VetsInTech\\itp_week_4\\day_1\\output.xlsx')
+
+sheet = wb['Sheet1']
+
+sheet['A1'] = "Symbol"
+sheet['A1'].font = Font(bold = True)
+sheet['B1'] =  'ROI'
+sheet['B1'].font = Font(bold = True)
+
 sym_counter = 0
+sheet_counter = 2
 
 for index in range(20):
     symbol_list = beautify['data'][sym_counter]['symbol']
+    sheet['A' + str(sheet_counter)] = symbol_list
+    sheet_counter += 1
     sym_counter += 1
-    print(symbol_list)
 
+
+sheet2_counter = 2
 roi_counter = 0
+
 for item in range(20):
     roi_list = beautify['data'][roi_counter]['metrics']['roi_data']['percent_change_last_1_week'] 
+    sheet['B' + str(sheet2_counter)] = roi_list
+    sheet2_counter += 1
     roi_counter += 1
-    print(roi_list)
 
 
+
+
+wb.save('C:\\Users\\GorkhaliSquad\\Documents\\VetsInTech\\itp_week_4\\day_1\\output.xlsx')
 
 
 
